a49e6612ce3da15e8fa67452d28c846f42afc465
LpxsBr <anselmolopes.an@gmail.com>
2023-04-27 10:28:32 -0300

    fix[style]: adjust global styles

--- src/components/buttons/lineButton.jsx
+++ src/components/buttons/lineButton.jsx
@@ -3,7 +3,7 @@ import './lineButton.css';
 function LineButton({...props}){
 
     return(
-        <a className='a-btn' href={props.redirectTo || '#'}>
+        <a className='secondary-btn' href={props.redirectTo || '#'}>
             {props.title || 'btn title'}
         </a>
     )
